3720ec01ad4cd43b8c39df28f78c1aed73b9cdac
罗春爱 <aizi031@126.com>
2016-09-16 18:25:40 +0800

    新增页面

---
+++ web/src/main/webapp/dist/js/yzm.js
@@ -0,0 +1,23 @@
+$(function  () {
+	//获取短信验证码
+	var validCode=true;
+	$(".yzm").click (function  () {
+		var time=30;
+		var code=$(this);
+		if (validCode) {
+			validCode=false;
+			code.addClass("msgs1");
+		var t=setInterval(function  () {
+			time--;
+			code.html(time+"秒");
+			if (time==0) {
+				clearInterval(t);
+			code.html("重新获取");
+				validCode=true;
+			code.removeClass("msgs1");
+
+			}
+		},1000)
+		}
+	})
+})
