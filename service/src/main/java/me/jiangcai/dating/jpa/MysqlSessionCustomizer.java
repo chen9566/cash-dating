ca9fe4e5ac6e2360aad34fd9668cc544aad0af33
Cai Jiang <luffy.ja@gmail.com>
2016-10-19 22:04:19 +0800

    apply into production.

--- service/src/main/java/me/jiangcai/dating/jpa/MysqlSessionCustomizer.java
+++ service/src/main/java/me/jiangcai/dating/jpa/MysqlSessionCustomizer.java
@@ -1,5 +1,7 @@
 package me.jiangcai.dating.jpa;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.eclipse.persistence.config.SessionCustomizer;
 import org.eclipse.persistence.sessions.Session;
 import org.eclipse.persistence.sessions.SessionEvent;
@@ -10,17 +12,28 @@ import org.eclipse.persistence.sessions.UnitOfWork;
  * @author CJ
  */
 public class MysqlSessionCustomizer implements SessionCustomizer {
+
+    private static final Log log = LogFactory.getLog(MysqlSessionCustomizer.class);
+
     @Override
     public void customize(Session session) throws Exception {
         session.getEventManager().addListener(new SessionEventAdapter() {
             @Override
-            public void postConnect(SessionEvent event) {
-                UnitOfWork work = event.getSession().acquireUnitOfWork();
+            public void preBeginTransaction(SessionEvent event) {
                 try {
-                    work.executeNonSelectingSQL("set names utf8mb4");
-                } finally {
-                    work.release();
+                    UnitOfWork work = event.getSession().acquireUnitOfWork();
+                    try {
+                        work.executeNonSelectingSQL("set names utf8mb4");
+                        work.commit();
+                        work = null;
+                    } finally {
+                        if (work != null)
+                            work.release();
+                    }
+                } catch (Exception ex) {
+                    log.error("UTF8MB4", ex);
                 }
+
             }
         });
     }
