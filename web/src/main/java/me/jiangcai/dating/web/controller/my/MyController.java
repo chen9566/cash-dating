b66613f374da3978f34dd97cc1904de625273dcf
Cai Jiang <luffy.ja@gmail.com>
2016-10-26 20:16:31 +0800

    mydata done.

---
+++ web/src/main/java/me/jiangcai/dating/web/controller/my/MyController.java
@@ -0,0 +1,38 @@
+package me.jiangcai.dating.web.controller.my;
+
+import me.jiangcai.dating.entity.User;
+import me.jiangcai.dating.service.StatisticService;
+import me.jiangcai.dating.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * @author CJ
+ */
+@Controller
+public class MyController {
+
+
+    @Autowired
+    private StatisticService statisticService;
+    @Autowired
+    private UserService userService;
+
+    @RequestMapping(method = RequestMethod.GET, value = {"/my"})
+    public String my(@AuthenticationPrincipal User user, Model model) {
+        model.addAttribute("user", userService.by(user.getId()));
+        model.addAttribute("total", statisticService.totalExpense(user.getOpenId()));
+//        model.addAttribute("orders", statisticService.countCashOrder(user.getOpenId()));
+        return "my.html";
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = {"/myData"})
+    public String data(@AuthenticationPrincipal User user, Model model) {
+        model.addAttribute("user", userService.by(user.getId()));
+        return "mydata.html";
+    }
+}
--- web/src/test/java/me/jiangcai/dating/page/MyDataPage.java
+++ web/src/test/java/me/jiangcai/dating/page/MyDataPage.java
@@ -1,17 +1,65 @@
 package me.jiangcai.dating.page;
 
+import me.jiangcai.dating.entity.User;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * @author CJ
  */
 public class MyDataPage extends AbstractPage {
+
+    private WebElement headImage;
+    private WebElement nameSpan;
+    private WebElement mobileSpan;
+    private WebElement toPayButton;
+
     public MyDataPage(WebDriver webDriver) {
         super(webDriver);
     }
 
     @Override
     public void validatePage() {
+        assertThat(webDriver.getTitle())
+                .isEqualTo("我的");
 
+        List<WebElement> rights = webDriver.findElements(By.cssSelector("span.pull-right"));
+
+        headImage = rights.get(0).findElements(By.tagName("img")).get(0);
+        nameSpan = rights.get(1);
+        mobileSpan = rights.get(2);
+        toPayButton = rights.get(3);// 有疑问
+
+
+        assertThat(headImage.isDisplayed()).isTrue();
+        assertThat(nameSpan.isDisplayed()).isTrue();
+        assertThat(mobileSpan.isDisplayed()).isTrue();
+        assertThat(toPayButton.isDisplayed()).isTrue();
+    }
+
+    /**
+     * 登出
+     */
+    public void logout() {
+        webDriver.findElements(By.tagName("input")).stream()
+                .filter(WebElement::isDisplayed)
+                .filter(webElement -> "注销".equals(webElement.getAttribute("value")))
+                .findFirst()
+                .orElseThrow(NullPointerException::new)
+                .click();
+    }
+
+    public void assertUser(User user) {
+        assertThat(nameSpan.getText())
+                .isEqualTo(user.getNickname());
+        assertThat(mobileSpan.getText())
+                .isEqualTo(user.getMobileNumber());
+        assertThat(headImage.getAttribute("src"))
+                .isEqualTo(user.getHeadImageUrl());
     }
 }
