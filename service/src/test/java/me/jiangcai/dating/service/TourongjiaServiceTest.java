7025cff88cc53969ab11f736d2731af880b55f4b
Cai Jiang <luffy.ja@gmail.com>
2016-10-31 11:45:02 +0800

    添加投融家申请借款测试

--- service/src/test/java/me/jiangcai/dating/service/TourongjiaServiceTest.java
+++ service/src/test/java/me/jiangcai/dating/service/TourongjiaServiceTest.java
@@ -1,11 +1,14 @@
 package me.jiangcai.dating.service;
 
 import me.jiangcai.dating.ServiceBaseTest;
+import me.jiangcai.dating.entity.User;
 import me.jiangcai.dating.model.trj.Loan;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.math.BigDecimal;
 import java.util.Arrays;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -23,6 +26,14 @@ public class TourongjiaServiceTest extends ServiceBaseTest {
         Loan[] loanList = tourongjiaService.loanList();
         System.out.println(Arrays.toString(loanList));
         assertThat(loanList)
+                .isNotEmpty();
+
+        User user = userService.byOpenId(createNewUser().getOpenId());
+
+        Loan loan = Stream.of(loanList).findAny().orElse(null);
+
+        String id = tourongjiaService.loan(loan, user, "轮渡人", new BigDecimal("5000"), "110000", "110103", "很复杂");
+        assertThat(id)
                 .isNotEmpty();
     }
 
