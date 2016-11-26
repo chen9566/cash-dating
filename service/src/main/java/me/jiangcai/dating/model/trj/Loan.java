6efe08f4b300fb9092542e394373c1ac0f5e2722
Cai Jiang <luffy.ja@gmail.com>
2016-11-26 16:51:41 +0800

    添加固定化的借款产品图标

--- service/src/main/java/me/jiangcai/dating/model/trj/Loan.java
+++ service/src/main/java/me/jiangcai/dating/model/trj/Loan.java
@@ -21,6 +21,20 @@ public class Loan {
     private String region;
     private String productName;
 
+    /**
+     * @return 产品图片的URI
+     */
+    public String getImageUri() {
+        if (productName.equals("快车宝"))
+            return "/images/Loan-icon.png";
+        if (productName.equals("随心贷"))
+            return "/images/Loan-icon1.png";
+        if (productName.equals("购车宝"))
+            return "/images/Loan-icon2.png";
+        //不知道什么图片的情况下
+        return "/images/Loan-con4.png";
+    }
+
     public String getAmount10K() {
         if (!amount.contains("万"))
             throw new IllegalArgumentException(amount);
