db6e8362c7698285fbe0419f7f6a9d579c7fa7e8
CJ <luffy.ja@gmail.com>
2016-12-01 17:56:13 +0800

    cdn

---
+++ cdn.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+
+if [[ ! $1 ]]; then
+    echo "$0 TargetHome"
+    exit 1
+fi
+if [[ ! -d $1 ]]; then
+    echo "$1 must be a folder"
+    exit 1
+fi
+if [[ ! -w $1 ]]; then
+    echo "can not write into $1"
+    exit 1
+fi
+
+if [[ ! -e $1/jquery-3.1.1.min.js ]]; then
+    wget http://code.jquery.com/jquery-3.1.1.min.js -O $1/jquery-3.1.1.min.js
+fi
+if [[ ! -e $1/jquery-ui.css ]]; then
+    wget http://code.jquery.com/ui/1.12.1/themes/eggplant/jquery-ui.css -O $1/jquery-ui.css
+fi
+if [[ ! -e $1/jquery-ui.min.js ]]; then
+    wget http://code.jquery.com/ui/1.12.1/jquery-ui.min.js -O $1/jquery-ui.min.js
+fi
+if [[ ! -e $1/bootstrap.min.css ]]; then
+    wget http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css -O $1/bootstrap.min.css
+fi
+if [[ ! -e $1/bootstrap-theme.min.css ]]; then
+    wget http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css -O $1/bootstrap-theme.min.css
+fi
+if [[ ! -e $1/bootstrap.min.js ]]; then
+    wget http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js -O $1/bootstrap.min.js
+fi
+if [[ ! -e $1/jquery-3.1.1.min.js ]]; then
+    wget http://code.jquery.com/jquery-3.1.1.min.js -O $1/jquery-3.1.1.min.js
+fi
+if [[ ! -e $1/jquery-3.1.1.min.js ]]; then
+    wget http://code.jquery.com/jquery-3.1.1.min.js -O $1/jquery-3.1.1.min.js
+fi
+if [[ ! -e $1/jquery-3.1.1.min.js ]]; then
+    wget http://code.jquery.com/jquery-3.1.1.min.js -O $1/jquery-3.1.1.min.js
+fi
+if [[ ! -e $1/jquery-3.1.1.min.js ]]; then
+    wget http://code.jquery.com/jquery-3.1.1.min.js -O $1/jquery-3.1.1.min.js
+fi
+if [[ ! -e $1/jquery-3.1.1.min.js ]]; then
+    wget http://code.jquery.com/jquery-3.1.1.min.js -O $1/jquery-3.1.1.min.js
+fi
+
+
+
