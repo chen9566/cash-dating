3aa181b78acda088164ff194cff24a11a9b70a23
Cai Jiang <luffy.ja@gmail.com>
2016-12-08 17:07:05 +0800

    财务也可以管理项目贷款.

--- service/src/main/java/me/jiangcai/dating/entity/support/ManageStatus.java
+++ service/src/main/java/me/jiangcai/dating/entity/support/ManageStatus.java
@@ -68,7 +68,7 @@ public enum ManageStatus {
                 list.addAll(Collections.singletonList((Role_Order_Value)));
                 break;
             case financial:
-                list.addAll(Arrays.asList(Role_Finance_Value, Role_Loan_Value));
+                list.addAll(Arrays.asList(Role_Finance_Value, Role_Loan_Value, Role_Project_Loan_Value));
                 break;
             case general:
                 list.addAll(Arrays.asList(Role_Finance_Value, Role_Loan_Value, Role_Project_Loan_Value, Role_Order_Value, Role_Edit_Value, Role_Agent_Value));
