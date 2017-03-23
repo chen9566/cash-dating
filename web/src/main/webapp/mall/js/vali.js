ef9c591489e20fe660d41bb1ee9c6fe67c5530c3
Cai Jiang <luffy.ja@gmail.com>
2017-03-23 10:10:50 +0800

    注册页完成

--- web/src/main/webapp/mall/js/vali.js
+++ web/src/main/webapp/mall/js/vali.js
@@ -1,24 +1,29 @@
-$.validator.setDefaults({
-    submitHandler: function () {
-        alert("注册成功!");
-    }
-});
+// $.validator.setDefaults({
+//     submitHandler: function () {
+//         alert("注册成功!");
+//     }
+// });
 
 $().ready(function () {
 // 在键盘按下并释放及提交后验证提交表单
     $("#signupForm").validate({
         rules: {
-            username: {
+            mobile: {
                 required: true,
-                minlength: 2
+                minlength: 11
+            },
+            verificationCode: {
+                required: true,
+                minlength: 4,
+                maxlength: 4,
             },
             password: {
                 required: true,
-                minlength: 5
+                minlength: 6
             },
             confirm_password: {
                 required: true,
-                minlength: 5,
+                minlength: 6,
                 equalTo: "#password"
             },
             agree: "required"
@@ -30,11 +35,11 @@ $().ready(function () {
             },
             password: {
                 required: "请输入密码",
-                minlength: "不能小于 5 位"
+                minlength: "不能小于 6 位"
             },
             confirm_password: {
                 required: "请输入密码",
-                minlength: "不能小于 5 位",
+                minlength: "不能小于 6 位",
                 equalTo: "两次密码不一致"
             },
             agree: "请接受我们的声明",
